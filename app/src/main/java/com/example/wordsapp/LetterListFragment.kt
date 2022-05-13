3d63e6a766c84c064c9128f14ecb8164b28cd02b
Sourabh Kumar <20010001030@gateway.edu.in>
2022-05-13 22:50:38 +0530

    Added onCreate ,onCreateView ,onViewCreated, onDestroyView

--- app/src/main/java/com/example/wordsapp/LetterListFragment.kt
+++ app/src/main/java/com/example/wordsapp/LetterListFragment.kt
@@ -1,7 +1,48 @@
 package com.example.wordsapp
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import com.example.wordsapp.databinding.FragmentLetterListBinding
+
+private val _binding : FragmentLetterListBinding? = null
+//If you're certain a value won't be null when you access it, you can append !! to its type name
+private val binding get() = _binding!!
+
+private lateinit var recycleView : RecyclerView
+
+
 
 class LetterListFragment : Fragment() {
+
+    //To display the options menu
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    //the layout is inflated in onCreateView()
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentLetterListBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        recycleView = binding.recyclerView
+        chooseLayout()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 
 }
